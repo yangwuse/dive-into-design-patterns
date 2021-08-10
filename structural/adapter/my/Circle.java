51cfde5b8f398258620f80d761e61c1f09ee216a
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-08-10 17:26:58 +0800

    m

---
+++ structural/adapter/my/Circle.java
@@ -0,0 +1,7 @@
+package structural.adapter.my;
+
+public class Circle {
+    private int radius;
+    public Circle(int r) { radius = r; }
+    public int getRadius() { return radius; }
+}
---
+++ structural/adapter/my/Square.java
@@ -0,0 +1,7 @@
+package structural.adapter.my;
+
+public class Square {
+    private int width;
+    public Square(int w) { width = w; }
+    public int getWidth() { return width; }
+}
